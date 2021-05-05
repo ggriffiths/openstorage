84aae08ea00781643b26b3c2fb86b492375fa900
ggriffiths <ggp493@gmail.com>
2021-05-05 20:46:31 +0000

    🔄 Synced local './' with remote 'staging/openstorage'

--- api/server/sdk/cloud_backup_test.go
+++ api/server/sdk/cloud_backup_test.go
@@ -24,7 +24,6 @@ import (
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/libopenstorage/openstorage/api"
-	api_err "github.com/libopenstorage/openstorage/api/errors"
 	"github.com/libopenstorage/openstorage/volume"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
@@ -132,48 +131,15 @@ func TestSdkCloudBackupCreateBadArguments(t *testing.T) {
 	s := newTestServer(t)
 	defer s.Stop()
 
-	id := "myvol"
-	uuid := "uuid"
-	taskId := "backup-task"
-	full := false
-	labels := map[string]string{"foo": "bar"}
-	busyReq := &api.SdkCloudBackupCreateRequest{
-		VolumeId:     id,
-		CredentialId: uuid,
-		Full:         full,
-		TaskId:       taskId,
-		Labels:       labels,
-		DeleteLocal:  true,
-	}
-	// Create response
-	s.MockDriver().
-		EXPECT().
-		CloudBackupCreate(&api.CloudBackupCreateRequest{
-			VolumeID:       id,
-			CredentialUUID: uuid,
-			Full:           false,
-			Name:           taskId,
-			Labels:         labels,
-			DeleteLocal:    true,
-		}).
-		Return(nil, api_err.ErrResourceBusy).
-		Times(1)
-	setupExpectedCredentialsPassing(s, uuid)
-
-	// Setup client
-	c := api.NewOpenStorageCloudBackupClient(s.Conn())
-	// resource busy
-	_, err := c.Create(context.Background(), busyReq)
-	serverError, ok := status.FromError(err)
-	assert.True(t, ok)
-	assert.Equal(t, serverError.Code(), codes.Unavailable)
-
 	req := &api.SdkCloudBackupCreateRequest{}
 	req.TaskId = "backup-task"
 
+	// Setup client
+	c := api.NewOpenStorageCloudBackupClient(s.Conn())
+
 	// volume id missing
-	_, err = c.Create(context.Background(), req)
-	serverError, ok = status.FromError(err)
+	_, err := c.Create(context.Background(), req)
+	serverError, ok := status.FromError(err)
 	assert.True(t, ok)
 	assert.Equal(t, serverError.Code(), codes.InvalidArgument)
 	assert.Contains(t, serverError.Message(), "volume id")
